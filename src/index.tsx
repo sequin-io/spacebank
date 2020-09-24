e06d528953f5f5f634b7cf3e6e89ce57919befd0
Anthony Accomazzo <accomazz@gmail.com>
2020-09-23 22:54:02 -0700

    Bump @decode/client to 0.2.5

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,7 @@ import { DecodeProvider } from "@decode/client";
 
 ReactDOM.render(
   <React.StrictMode>
-    <DecodeProvider>
-      <App />
-    </DecodeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
